b2f6eba47167a4c36f39c7535db8b13856f4598d
NickahlisKopel <nickahliskopel.student@careerdevs.com>
2022-01-04 18:40:22 -0500

    update

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -1,10 +1,12 @@
 package com.careerdevs;
 
+import com.careerdevs.intro.ControlFlow;
 import com.careerdevs.intro.DataTypes;
 
 public class Main {
 
     public static void main(String[] args) {
-        DataTypes.main();
+        //DataTypes.main();
+        ControlFlow.main();
     }
 }
--- src/com/careerdevs/intro/ControlFlow.java
+++ src/com/careerdevs/intro/ControlFlow.java
@@ -1,7 +1,47 @@
 package com.careerdevs.intro;
 
 public class ControlFlow {
-    public static void main(String[] args) {
+    public static void main() {
+
+        //Comparison Operators------------------------------------------------
+        int x = 1;
+        int y = 1;
+//        System.out.println(x == y);
+//        System.out.println(x != y);
+//        System.out.println(x < y);
+//        System.out.println(x > y);
+//        System.out.println(x >= y);
+//        System.out.println(x <= y);
+
+        //Logical Operators -------------------------------------------------
+        int temperature = 22;
+        boolean isWarm = temperature > 20 && temperature < 30;
+        System.out.println(isWarm);
+
+        boolean hasHighIncome = true;
+        boolean hasGoodCredit = true;
+        boolean hasCriminalRecord = false;
+        boolean isEligible = (hasHighIncome || hasGoodCredit) && !hasCriminalRecord;
+
+        //If Statements --------------------------------------------------------------
+        int temp = 32;
+        if (temp > 30) {
+            System.out.println("Its a hot day");
+            System.out.println("Drink water");
+        } else if (temp > 20) {
+            System.out.println("Beautiful day");
+        } else {
+            System.out.println("Cold day");
+        }
+
+        //Simplifying If Statements -------------------------------------
+        int income = 120_000;
+        boolean hasHighIncome02 = (income > 100_000);
+
+        //For Loops ------------------------------------------------------
 
     }
+
+
 }
+
